5a4f29ba2b92e2b8e7fa2c123bdcc0d2981e4fdf
Musafak <musafak.ft18@mail.umy.ac.id>
2020-06-16 19:24:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/asmaul/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.asmaul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private static int splashInternal = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ShimmerFrameLayout container = (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
+        container.startShimmerAnimation();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, ActivityAsmaulHusna.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splashInternal);
+    }
+}
